7002b716fb6172b8e96ce18695ff27353ae83ac6
matheusedivado <ms1501085@gmail.com>
2024-10-18 18:03:16 -0300

    Adiciona animaçaõ no NavBar

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,29 +1,40 @@
 import React, { useState } from 'react';
 import styles from './Navbar.module.css';
-import { getImageUrl } from "../../utils"
+import { getImageUrl } from "../../utils";
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const [fadeOut, setFadeOut] = useState(false);
+
+    const handleMenuToggle = () => {
+        if (menuOpen) {
+            setFadeOut(true);
+            setTimeout(() => {
+                setMenuOpen(false);
+                setFadeOut(false);
+            }, 200);
+        } else {
+            setMenuOpen(true);
+        }
+    };
+
     return (
         <nav className={styles.navbar}>
             <a className={styles.title} href="/">Me</a>
             <div className={styles.menu}>
-                <img className={styles.menuBtn}
-                    src={
-                        menuOpen
-                            ? getImageUrl("nav/closeIcon.png")
-                            : getImageUrl("nav/menuIcon.png")
-                    }
+                <img
+                    className={styles.menuBtn}
+                    src={menuOpen ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")}
                     alt='btn-menu'
-                    onClick={() => setMenuOpen(!menuOpen)} />
+                    onClick={handleMenuToggle}
+                />
                 <ul
-                    className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-                    onClick={() => setMenuOpen(false)}
+                    className={`${styles.menuItems} ${menuOpen ? styles.menuOpen : ''} ${fadeOut ? styles.fadeOut : ''}`}
                 >
-                    <li><a href="#about">Sobre mim</a></li>
-                    <li><a href="#skills">Habilidades</a></li>
-                    <li><a href="#projects">Projetos</a></li>
-                    <li><a href="#contact">Contato</a></li>
+                    <li className={fadeOut ? 'fadeOut' : ''}><a href="#about">Sobre mim</a></li>
+                    <li className={fadeOut ? 'fadeOut' : ''}><a href="#skills">Habilidades</a></li>
+                    <li className={fadeOut ? 'fadeOut' : ''}><a href="#projects">Projetos</a></li>
+                    <li className={fadeOut ? 'fadeOut' : ''}><a href="#contact">Contato</a></li>
                 </ul>
             </div>
         </nav>
