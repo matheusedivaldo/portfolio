1616161316a29eaf286c974e493980b3dfc57b54
matheusedivado <ms1501085@gmail.com>
2024-10-18 17:54:38 -0300

    Finaliza componente NavBar

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Navbar.module.css';
+import { getImageUrl } from "../../utils"
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
         <nav className={styles.navbar}>
-            <a className={styles.title} href="/">Portfólio</a>
+            <a className={styles.title} href="/">Me</a>
             <div className={styles.menu}>
-                <ul className={styles.menuItems}>
+                <img className={styles.menuBtn}
+                    src={
+                        menuOpen
+                            ? getImageUrl("nav/closeIcon.png")
+                            : getImageUrl("nav/menuIcon.png")
+                    }
+                    alt='btn-menu'
+                    onClick={() => setMenuOpen(!menuOpen)} />
+                <ul
+                    className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                    onClick={() => setMenuOpen(false)}
+                >
                     <li><a href="#about">Sobre mim</a></li>
                     <li><a href="#skills">Habilidades</a></li>
                     <li><a href="#projects">Projetos</a></li>
