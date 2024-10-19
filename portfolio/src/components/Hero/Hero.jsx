d42e06d4330b89c70e5a71265ab7bfd2cff9b188
matheusedivado <ms1501085@gmail.com>
2024-10-18 22:04:04 -0300

    Troca botão Vamos Conversar por ícones das Redes Sociais

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
+import { FaGithub, FaLinkedin, FaInstagram, FaEnvelope, FaFacebook } from "react-icons/fa";
 
 export const Hero = () => {
     return (
@@ -16,9 +16,23 @@ export const Hero = () => {
                     <span className={styles.php}> PHP</span>,
                     busco sempre aprimorar minhas habilidades e contribuir para soluções eficazes.
                 </p>
-                <a href="mailto:matheusedivaldodev@gmail.com" className={styles.contactBtn}>
-                    Vamos conversar!
-                </a>
+                <div className={styles.socialIcons}>
+                    <a href="https://github.com/matheusedivaldo" target="_blank" rel="noopener noreferrer">
+                        <FaGithub className={styles.icon} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/matheus-silva-8297a320a/" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin className={styles.icon} />
+                    </a>
+                    <a href="https://www.instagram.com/dev_startando" target="_blank" rel="noopener noreferrer">
+                        <FaInstagram className={styles.icon} />
+                    </a>
+                    <a href="mailto:matheusedivaldodev@gmail.com" target="_blank" rel="noopener noreferrer">
+                        <FaEnvelope className={styles.icon} />
+                    </a>
+                    <a href="https://www.facebook.com/profile.php?id=100016363537300&locale=pt_BR" target="_blank" rel="noopener noreferrer">
+                        <FaFacebook className={styles.icon} />
+                    </a>
+                </div>
             </div>
             <img
                 src={getImageUrl("hero/heroImage.png")}
