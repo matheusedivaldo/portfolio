99e690a4baf86ff8fdd81b5eb115c598725868fc
matheusedivado <ms1501085@gmail.com>
2024-10-27 14:27:56 -0300

    Ajustes

--- portfolio/src/components/ServiceList/ServiceList.jsx
+++ portfolio/src/components/ServiceList/ServiceList.jsx
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaChevronDown, FaChevronUp  } from "react-icons/fa6";
+
 import services from "../../data/services.json";
 import styles from "./ServiceList.module.css";
 
 const Services = () => {
+    const [expandedCategories, setExpandedCategories] = useState({});
+    const [expandedServices, setExpandedServices] = useState({});
+
+    // Alterna a visualização das categorias
+    const toggleCategory = (index) => {
+        setExpandedCategories((prevState) => ({
+            ...prevState,
+            [index]: !prevState[index],
+        }));
+    };
+
+    // Alterna a visualização das descrições dos serviços
+    const toggleService = (categoryIndex, serviceIndex) => {
+        const key = `${categoryIndex}-${serviceIndex}`;
+        setExpandedServices((prevState) => ({
+            ...prevState,
+            [key]: !prevState[key],
+        }));
+    };
+
     return (
         <div className={styles.container} id="services">
             <h2>Serviços</h2>
             <div className={styles.servicesList}>
                 {services.map((category, index) => (
                     <div key={index} className={styles.serviceCategory}>
-                        <h3>{category.category}</h3>
-                        <ul>
-                            {category.services.map((service, serviceIndex) => (
-                                <li key={serviceIndex}>{service}</li>
-                            ))}
-                        </ul>
+                        <h3 onClick={() => toggleCategory(index)}>
+                            {category.category}
+                            {expandedCategories[index] ? <FaChevronUp /> : <FaChevronDown />}
+                        </h3>
+                        {expandedCategories[index] && (
+                            <ul>
+                                {category.services.map((service, serviceIndex) => (
+                                    <li key={serviceIndex} onClick={() => toggleService(index, serviceIndex)}>
+                                        <span>{service.title}</span>
+                                        {expandedServices[`${index}-${serviceIndex}`] && (
+                                            <p className={styles.serviceDescription}>{service.description}</p>
+                                        )}
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
                     </div>
                 ))}
             </div>
