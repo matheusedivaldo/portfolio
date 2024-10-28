c10e6d6b41b9e393a975d0a324e4962266a7ab69
matheusedivado <ms1501085@gmail.com>
2024-10-28 18:49:08 -0300

    Remove icon do lado de cada description de services

--- portfolio/src/components/ServiceList/ServiceList.jsx
+++ portfolio/src/components/ServiceList/ServiceList.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { FaCode, FaPaintBrush } from "react-icons/fa";
 import servicesData from "../../data/services.json";
 import styles from "./ServiceList.module.css";
 
@@ -10,24 +9,12 @@ const Services = () => {
         setExpandedService(expandedService === index ? null : index);
     };
 
-    const getCategoryIcon = (category) => {
-        switch (category) {
-            case "Desenvolvimento":
-                return <FaCode className={styles.icon} />;
-            case "Design":
-                return <FaPaintBrush className={styles.icon} />;
-            default:
-                return null;
-        }
-    };
-
     return (
         <section className={styles.container} id="services">
             <h2 className={styles.title}>Serviços</h2>
             {servicesData.map((categoryData, catIndex) => (
                 <div key={catIndex} className={styles.categorySection}>
                     <div className={styles.categoryHeader}>
-                        {getCategoryIcon(categoryData.category)}
                         <h3 className={styles.categoryTitle}>{categoryData.category}</h3>
                     </div>
                     <div className={styles.cardsContainer}>
