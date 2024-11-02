fba367172465e7778d76d8b2125d54c6119bfad4
matheusedivado <ms1501085@gmail.com>
2024-11-01 22:47:41 -0300

    Framer Motion - Hero

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 import { FaGithub, FaLinkedin, FaInstagram, FaEnvelope, FaFacebook } from "react-icons/fa";
@@ -7,14 +8,31 @@ export const Hero = () => {
     return (
         <section className={styles.container}>
             <div className={styles.content}>
-                <h1 className={styles.title}>Olá, sou Matheus</h1>
-                <p className={styles.description}>
+                <motion.h1
+                    className={styles.title}
+                    initial={{ opacity: 0, x: -50 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.5 }}
+                >
+                    Olá, sou Matheus
+                </motion.h1>
+                <motion.p
+                    className={styles.description}
+                    initial={{ opacity: 0, x: -30 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.6 }}
+                >
                     Desenvolvedor júnior com 2 anos de experiência em sistemas ERP.
                     Possuo conhecimento em <span className={styles.frontend}>Front-end</span> e
                     <span className={styles.backend}> Back-end</span>, buscando sempre aprimorar
                     minhas habilidades e contribuir para soluções eficazes.
-                </p>
-                <div className={styles.socialIcons}>
+                </motion.p>
+                <motion.div
+                    className={styles.socialIcons}
+                    initial={{ opacity: 0, x: -20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.7 }}
+                >
                     <a href="https://github.com/matheusedivaldo" target="_blank" rel="noopener noreferrer">
                         <FaGithub className={styles.icon} />
                     </a>
@@ -30,12 +48,15 @@ export const Hero = () => {
                     <a href="https://www.facebook.com/profile.php?id=100016363537300&locale=pt_BR" target="_blank" rel="noopener noreferrer">
                         <FaFacebook className={styles.icon} />
                     </a>
-                </div>
+                </motion.div>
             </div>
-            <img
+            <motion.img
                 src={getImageUrl("hero/heroImage.png")}
                 alt="Minha imagem"
                 className={styles.heroImg}
+                initial={{ opacity: 0, x: -50 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.8 }}
             />
         </section>
     );
